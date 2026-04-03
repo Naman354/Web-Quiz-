fe9d1d4fe1e18a8b2794caf2b55e80a029da3b54
Vinay Papnoi <papnoi.vinay@gmail.com>
2026-04-04 00:49:53 +0530

    Update quiz questions and start function

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const questions = [
     {
         question: "Which is the largest animal in the world?",
         answers: [
-            { text: "Shark", correct: false},
+            { text: "T-Rex", correct: false},
             { text: "Blue Whale", correct: true},
             { text: "Elephant", correct: false},
             { text: "Giraffe", correct: false},
